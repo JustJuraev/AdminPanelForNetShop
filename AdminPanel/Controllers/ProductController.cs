f62d44319dd290530fa4d51a63cbda2265c9ab76
Abdusamad Juraev <juraevabdusamad517@gmail.com>
2023-01-02 13:29:11 +0500

    Task 7, 8: Add a product model and added product controller

---
+++ AdminPanel/Controllers/ProductController.cs
@@ -0,0 +1,15 @@
+﻿using AdminPanel.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace AdminPanel.Controllers
+{
+    public class ProductController : Controller
+    {
+      
+        public IActionResult Products()
+        {
+            return View();
+        }
+    }
+}
---
+++ AdminPanel/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿namespace AdminPanel.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Image { get; set; }
+
+        public int Price { get; set; }
+
+        public string ShortDesc { get; set; }
+
+        public string LongDesc { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public int Count { get; set; }
+
+    }
+}
